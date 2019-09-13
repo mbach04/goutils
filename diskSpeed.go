0bc0ba034c49af25afa55881ab63ffbd0feb86d7
matthew bach <mbach@fedora.localdomain>
2019-09-12 21:50:35 -0400

    add diskSpeed util

---
+++ diskSpeed.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+)
+
+var fileSize = "10M"
+var tmpFile = "/run/media/mbach/USB-Gadget/.t10M"
+var count = 1000
+
+func main() {
+	cmd := fmt.Sprintf("head -c %s </dev/urandom > %s", fileSize, tmpFile)
+	// fmt.Println(cmd)
+	waitWithCombinedOut(cmd)
+	b, err := ioutil.ReadFile(tmpFile)
+	if err != nil {
+		fmt.Println("Error reading file: %v", err)
+		return
+	}
+	start := time.Now()
+	writeFileNTimes(tmpFile, &b, count)
+	elapsed := time.Since(start)
+	deleteFile(tmpFile)
+	fmt.Println("Finished", count, "cycles of", fileSize)
+	fmt.Println("Time Elapsed:", elapsed)
+
+}
+
+func writeFileNTimes(path string, b *[]byte, n int) {
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println("Error opening file: %v", err)
+		return
+	}
+	defer file.Close()
+
+	for i := 1; i <= n; i++ {
+		fmt.Println(i, "of", count)
+		_, err = file.Write(*b)
+		if err != nil {
+			fmt.Println("Error writing to file: %v", err)
+			return
+		}
+	}
+
+	// save changes
+	err = file.Sync()
+	if err != nil {
+		fmt.Println("Error syncing file before close: %v", err)
+		return
+	}
+
+	fmt.Sprintln("Finished writing to tmp file: %s", tmpFile)
+}
+
+func deleteFile(path string) {
+	var err = os.Remove(path)
+	if err != nil {
+		fmt.Println("Error deleting file: %v", err)
+		return
+	}
+}
+
+func waitWithCombinedOut(s string) {
+	cmd := exec.Command("sh", "-c", s)
+
+	_, err := cmd.CombinedOutput() //wait for return
+	if err != nil {
+		log.Printf("Command failed with error: %v", err)
+	}
+
+	// fmt.Println(b)
+}
